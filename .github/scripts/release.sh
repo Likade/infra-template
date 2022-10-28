b49754f4e8abe0c5f9de6a5167b089a2cf419858
Likade <lera24072@gmail.com>
2022-10-29 01:16:12 +0300

    hello

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -8,12 +8,12 @@ current_tag=$(git tag | tail -1 | head -n1)
 previous_tag=$(git tag | tail -2 | head -n1)
 
 author=$(git show ${current_tag} | grep Author: | head -1)
-date=$(git show ${current_tag}| grep Date: | head -1)
+date=$(git show ${current_tag} --date=short| grep Date:)
 changeLog=`git log "${previous_tag}"..${current_tag} --pretty=format:"\n* %h -- %an -- %s;" | tr -s "\n" " "`
 description="ответственный за релиз ${author}\n коммиты, попавшие в релиз:\n ${changeLog} \n ${comment}"
 
 code=$(curl -w "%{http_code}\\n" \
--d '{"summary": "'"Релиз ${current_tag} - ${date}"'","description": "'"${description}"'"}' \
+-d '{"summary": "'"Релиз ${current_tag} - ${date:5}"'","description": "'"${description}"'"}' \
 -H "Content-Type: application/json"  \
 -H "Authorization: OAuth ${OAuth}" \
 -H "X-Org-ID: ${OrgId}" \
